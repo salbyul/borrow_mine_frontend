b5f757dc32f8cb0e29c352bc9f468a3d99d89a45
salbyul <kpast96@gmail.com>
2023-04-16 16:51:41 +0900

    Profile Bookmark

--- src/components/profile/ProfileBookmark.js
+++ src/components/profile/ProfileBookmark.js
@@ -1,7 +1,31 @@
+import axios from 'axios';
+import BorrowComponent from '../borrow/BorrowComponent';
+import { useEffect, useState } from 'react';
+
 function ProfileBookmark() {
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('/borrow/bookmark')
+            .then((response) => {
+                console.log(response);
+                setList(response.data.borrowPosts);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
     return (
         <>
-            <div>His</div>
+            <div className="mt-5">
+                <h1 className="text-xl mb-7 text-gray-700">즐겨찾기</h1>
+                <div className="flex flex-col items-center">
+                    {list.map((c, idx) => {
+                        return <BorrowComponent key={idx} post={c} />;
+                    })}
+                </div>
+            </div>
         </>
     );
 }
